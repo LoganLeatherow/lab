2f83b1efa11d3170de8d47765985fa32671867fb
Logan Leatherow <lleatherow@niagarawater.com>
2022-04-16 17:08:23 -0500

    problem 6 done

--- lab.js
+++ lab.js
@@ -96,7 +96,11 @@ for (let i = 0; i < message.length + 5; i++) {
 */
 
 //CODE HERE
-
+let array = [1,2,3,4,5,6,7,8,9,10]
+for (let index = 0; index < 10; index++) {
+  console.log (array[index])
+  
+}
 ////////// PROBLEM 7 //////////
 
 /*
